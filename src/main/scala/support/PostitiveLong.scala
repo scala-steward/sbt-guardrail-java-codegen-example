91dd9e41daab1a2adef45b36f17d277092095ea9
Sean C. Sullivan <github@seansullivan.com>
2019-11-04 10:46:18 -0500

    remove PositiveLong.scala

--- src/main/scala/support/PostitiveLong.scala
+++
@@ -1,9 +0,0 @@
-package support
-
-import com.example.clients.petstore.Implicits
-
-class PositiveLong private (val value: Long) extends AnyVal
-
-object PositiveLong {
-  def apply(value: Long): Option[PositiveLong] = if (value >= 0) Some(new PositiveLong(value)) else None
-}
